e405a50a4078c7916b252647916d2d11c9cf75d8
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-02-23 09:53:59 +0330

    Completed the tasks and tested them

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -3,8 +3,8 @@ public class Exercises1 {
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        if(n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
     }
 
 
@@ -13,8 +13,8 @@ public class Exercises1 {
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        if(n == 1 || n == 2) {return 1;}
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
 
@@ -29,9 +29,18 @@ public class Exercises1 {
     *****
 
      */
-    public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+    public char[][] generateTriangle(int rows)
+    {
+        char[][] result = new char[rows][];
+        for (int i = 0; i < rows; i++)
+        {
+            result[i] = new char[i + 1];
+            for (int j = 0; j <= i; j++)
+            {
+                result[i][j] = '*';
+            }
+        }
+        return result;
     }
 
 
